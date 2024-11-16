3bf51b5bedd871f350a39918d4060c6dfa5e24e4
Mateus Santos <mateuso12@hotmail.com.br>
2024-11-16 11:53:40 -0300

    Initial commit

---
+++ backend/src/main/java/com/projeto/integrado/repository/StatusTarefaRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.integrado.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.integrado.entity.StatusTarefa;
+
+public interface StatusTarefaRepository extends JpaRepository<StatusTarefa, Integer> {
+	Optional<Projeto> findByStatusTarefa(String StatusTarefa);
+}
